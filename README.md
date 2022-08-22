c638d68c4fdde98f41541922e632e273ce6c000d
Awen <awengrhys@gmail.com>
2022-08-22 16:07:39 +0100

    mk2 commands but no input

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ArachnidRobots
 
+# MK1
 Command is recieved via the console and is in the format: initX,initY,movements
 
 mk1.js is run using "node mk1.js" in the terminal, and will first run test cases before asking for a command input
@@ -8,3 +9,19 @@ Possible returns:
 [x,y]: final location
 'Invalid input for initial location!': initX,initY weren't numbers and hence are invalid as an initial location
 'Invalid Movement Command!': movement command which wasn't F, B, L or R were entered in the control sequence
+
+# MK2
+
+Assumptions:
+There's no longer a B command (might get very confusing if you want to go backwards twice in a row!)
+The bot is solely navigating an X,Y sized glass pane - it doesn't want to go back onto the ground 
+Want something in the logs for when bot is stopped going off the edge
+
+Command Structure:
+initX,initY,initFacing,buildingSizeX,buildingSizeY,commands
+initX: initial X location of bot
+initY: initial Y location of bot
+initFacing: direction bot is initially facing of N,E,S,W (North, East, South, West)
+buildingSizeX: length of glass (inclusive; the bot can safely go to (buildingSizeX, buildingSizeY))
+buildingSizeY: height of glass
+commands: string of characters F, L & R denoting the instructions to the bot
---
+++ robots.js
@@ -0,0 +1,175 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+const mk1 = {
+    name: 'mk2',
+    location: [0,0],
+    recieveCommand(command) {
+        const splitCommand = command.split(",");
+        if(!(isNaN(splitCommand[0]) || isNaN(splitCommand[1]))) {
+            this.location[0] = splitCommand[0];
+            this.location[1] = splitCommand[1];
+        } else {
+            return 'Invalid input for initial location!';
+        }
+
+        for(let i = 0; i < splitCommand[2].length; i++) {
+            switch(splitCommand[2][i]) {
+                case "F":
+                    this.forward();
+                    break;
+                case "B":
+                    this.backwards();
+                    break;
+                case "L":
+                    this.left();
+                    break;
+                case "R":
+                    this.right();
+                    break;
+                default:
+                    return 'Invalid Movement Command!';
+            }
+        } 
+
+        return this.location;
+    },
+    forward() {
+        this.location[1]++;
+    },
+    backwards() {
+        this.location[1]--;
+    },
+    left() {
+        this.location[0]--;
+    },
+    right() {
+        this.location[0]++;
+    }
+}
+
+const compass = ['N', 'E', 'S', 'W']
+
+const mk2 = {
+    name: 'mk2',
+    location: [0,0],
+    facing: 0,
+    buildingSize: [0,0],
+    recieveCommand(command) {
+        const splitCommand = command.split(",");
+        if(!(isNaN(splitCommand[0]) || isNaN(splitCommand[1]))) {
+            this.location[0] = splitCommand[0];
+            this.location[1] = splitCommand[1];
+        } else {
+            return 'Invalid input for initial location!';
+        }
+
+        this.facing = compass.indexOf(splitCommand[2].toUpperCase());
+        if(this.facing == -1) {
+            return 'Invalid initial direction facing!';
+        }
+
+        if(!(isNaN(splitCommand[3]) || isNaN(splitCommand[4]))) {
+            this.buildingSize[0] = splitCommand[3];
+            this.buildingSize[1] = splitCommand[4];
+        } else {
+            return 'Invalid input for initial location!';
+        }
+
+        for(let i = 0; i < splitCommand[5].length; i++) {
+            switch(splitCommand[5][i]) {
+                case "F":
+                    this.forward();
+                    break;
+                case "L":
+                    this.left();
+                    break;
+                case "R":
+                    this.right();
+                    break;
+                default:
+                    return 'Invalid Movement Command!';
+            }
+        } 
+
+        return this.location;
+    },
+    forward() {
+        switch(compass[this.facing]) {
+            case 'N':
+                if(this.location[1] < this.buildingSize[1]) {
+                    this.location[1]++;
+                } else {
+                    console.log('Nearly killed the bot off the top!');
+                }
+                break;
+            case 'E':
+                if(this.location[0] < this.buildingSize[0]) {
+                    this.location[0]++;
+                } else {
+                    console.log('Nearly killed the bot off the right side!');
+                }
+                break;
+            case 'S':
+                if(this.location[1] > 0) {
+                    this.location[1]--;
+                } else {
+                    console.log('Nearly killed the bot off the bottom!');
+                }
+                break;
+            case 'W':
+                if(this.location[0] > 0) {
+                    this.location[0]--;
+                } else {
+                    console.log('Nearly killed the bot off the left side!');
+                }
+                break;
+        }
+    },
+    left() {
+        this.facing = (this.facing - 1) % 4 
+    },
+    right() {
+        this.facing = (this.facing + 1) % 4
+    }
+}
+
+const testFunction = (testObj, testCommands, testResults) => {
+    console.log("Testing " + testObj.name);
+    for(let i = 0; i < testCommands.length; i++) {
+        const actual = testObj.recieveCommand(testCommands[i])
+        //console.log(actual.length);
+        if(actual.length > 2) {
+            if(actual === testResults[i]) {
+                console.log(`Test ${i} passed`)
+            } else {
+                console.log(`Test ${i} failed with expected: ${testResults[i]} and actual: ${actual}`)
+            }
+        } else if(actual[0] == testResults[i][0] && actual[1] == testResults[i][1]) {
+            console.log(`Test ${i} passed`);
+        } else {
+            console.log(`Test ${i} failed with expected: ${testResults[i]} and actual: ${actual}`)
+        }
+    }
+
+    console.log("Test complete!");
+}
+
+const testCommands = ["0,0,FRFRFFFFFFFLLLLFFFFFRFFFFLFFLRRF", "3,6,FFFFFFFFRRRRRRRFFFFLLLBBRRRRRLLLLLLLLLRFFF", 
+            "0,7,RRRRRRRRFFFFFFFFFFFLLLBBBBBRRRLLLLLFFLR", "A,B,FFBRL", "0,1,FFGHTBSHFJK", "100,100,FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR"];
+const testResults = [[-1, 21], [4, 19], [3,15], 'Invalid input for initial location!', 'Invalid Movement Command!', [191, 191]]
+
+const testCommands2 = ["0,0,N,2,2,FFFF", "0,1,S,4,4,LFFRF", "1,5,S,6,6,RFFLFFFFFFF"];
+const testResults2 = [[0,2], [2,0], [0,0]];
+
+testFunction(mk1, testCommands, testResults);
+testFunction(mk2, testCommands2, testResults2);
+
+/*rl.question("Please insert a command: " , function (command) {
+  console.log(`Bot ends up at: ${mk1.recieveCommand(command)}`);
+  rl.close();
+});*/
