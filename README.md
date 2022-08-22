22cb9f9e2f0b0cbf5c9944f8b671c55b8b860106
Awen <awengrhys@gmail.com>
2022-08-22 14:56:49 +0100

    mk1 commit
    Improper input not yet dealt with, but runs 3 test cases before asking for input

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ArachnidRobots
+
+Command is recieved via the console and is in the format: initX,initY,movements
+
+mk1.js is run using "node mk1.js" in the terminal, and will first run test cases before asking for a command input
+
+Edge cases of improper input not yet dealt with
--- mk1.js
+++ mk1.js
@@ -1,3 +1,10 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
 const mk1 = {
     location: [0,0],
     recieveCommand(command) {
@@ -41,10 +48,25 @@ const mk1 = {
     }
 }
 
-let commands = ["0,0,FRFRFFFFFFFLLLLFFFFFRFFFFLFFLRRF", "3,6,FFFFFFFFRRRRRRRFFFFLLLBBRRRRRLLLLLLLLLRFFF", 
+let testCommands = ["0,0,FRFRFFFFFFFLLLLFFFFFRFFFFLFFLRRF", "3,6,FFFFFFFFRRRRRRRFFFFLLLBBRRRRRLLLLLLLLLRFFF", 
             "0,7,RRRRRRRRFFFFFFFFFFFLLLBBBBBRRRLLLLLFFLR"];
+let testResults = [[-1, 21], [4, 19], [3,15]]
 
+console.log("Testing..");
 
-for(let i = 0; i < commands.length; i++) {
-    console.log(mk1.recieveCommand(commands[i]));
+for(let i = 0; i < testCommands.length; i++) {
+    const actual = mk1.recieveCommand(testCommands[i])
+    if(actual[0] == testResults[i][0] && actual[1] == testResults[i][1]) {
+        console.log(`Test ${i} passed`);
+    } else {
+        console.log(`Test ${i} failed with expected: ${testResults[i]} and actual: ${actual}`)
+    }
 }
+
+console.log("Test complete!");
+
+
+rl.question("Please insert a command: " , function (command) {
+  console.log(`Bot ends up at: ${mk1.recieveCommand(command)}`);
+  rl.close();
+});
